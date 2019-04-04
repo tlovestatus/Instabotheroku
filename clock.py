2fe2d031f1657624efe8bcb8880f6027925d1aaf
tlovestatus <48553699+tlovestatus@users.noreply.github.com>
2019-04-04 12:32:55 +0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -15,21 +15,22 @@ set_workspace(path="./")
 workspace_in_use = get_workspace()
 print(workspace_in_use["path"])
 
-# get an InstaPy session!
-session = InstaPy(username=os.environ['username'],
-                  password=os.environ['password'],
-                  headless_browser=True)
-with smart_run(session):
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', hour=os.environ['hour'],minute=os.environ['minute'])
+def scheduled_job():
+  # get an InstaPy session!
+  session = InstaPy(username=os.environ['username'],
+                    password=os.environ['password'],
+                    headless_browser=True)
+  with smart_run(session):
         """ Activity flow """
         # general settings
         session.set_dont_include(["friend1", "friend2", "friend3"])
         #session.follow_likers(follow_likers_of_users, photos_grab_amount=int(os.environ['photos_grab_amount']), follow_likers_per_photo=int(os.environ['follow_likers_per_photo']), randomize=False, sleep_delay=int(os.environ['follow_sleep_delay']), interact=False)
         session.unfollow_users(amount=int(os.environ['unfollow_amount']), allFollowing=True, style="LIFO", unfollow_after=48 * 60 * 60, sleep_delay=int(os.environ['unfollow_sleep_delay']))
         # activity
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', hour=os.environ['hour'],minute=os.environ['minute'])
-def scheduled_job():
         #session.like_by_tags(["natgeo"], amount=10)
 
 sched.start()
